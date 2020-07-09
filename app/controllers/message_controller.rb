407870caa0ee4e1c918f3218bf06ffeb2566e305
Mahe2809 <mahe2809pro@gmail.com>
2020-07-09 17:05:04 +0530

    Chat Application using HTTP requests

---
+++ app/controllers/message_controller.rb
@@ -0,0 +1,21 @@
+class MessageController < ApplicationController
+  
+  before_action :required_user
+  
+  def create
+    # byebug
+    message = current_user.messages.build(message_params)
+    if message.save
+      redirect_to root_path
+    end
+
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:body)
+  end
+  
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get 'login', to:'session#new'
   post 'login', to: 'session#create'
   delete 'logout', to: 'session#destroy'
+  post 'message', to: 'message#create'
 end
