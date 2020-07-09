5e9d1d2690a3b08ec307271241306abc0bf603c4
Mahe2809 <mahe2809pro@gmail.com>
2020-07-09 11:53:01 +0530

    DB Migration files

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# User.create(username:"MS Dhoni", password:"password")
+# User.create(username:"Jon Snow", password:"password")
+# User.create(username:"Harry Potter", password:"password")
+# User.create(username:"Katniss Everdeen", password:"password")
+# User.create(username:"Robert Langdon", password:"password")
