8762ba7c84eac4a8986e0a150516e9a3a561e33c
Mahe2809 <mahe2809pro@gmail.com>
2020-07-09 13:36:10 +0530

    Login functionalities and session control

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -1,8 +1,17 @@
 class ChatroomController < ApplicationController
 
     def index
-        
+        @messages = Message.all
     end
     
 
 end
+
+
+#<div class="event">
+# <div class="content">
+# <div class="summary me">
+    # Hello!
+# </div>
+# </div>
+# </div>
--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,6 +1,27 @@
 class SessionController < ApplicationController
 
     def new
+
+    end
+    def create
+        # byebug
+        user = User.find_by(username:params[:session][:username])
+
+        if user && user.authenticate(params[:session][:password])
+            session[:user_id] = user.id
+            flash[:notice] = "Logged In Successfully"
+            redirect_to root_path
+        else
+            flash.now[:alert] = "Wrong Credentials (OR) User does not exist"
+            render 'new'
+        end
+
+    end
+
+    def destroy
+        session[:user_id] = nil
+        flash[:notice] = "Logged Out!"
+        redirect_to login_path
     end
 
 end
